4d65c765001a77d3019e302152e571398b7200ba
Billy <billychorey@gmail.com>
2023-09-12 11:08:06 -0400

    cleanup

--- index.js
+++ index.js
@@ -7,7 +7,105 @@ document.addEventListener("DOMContentLoaded", (event) => {
     let dogBreeds = [];
     const breedInput = document.getElementById('input');
 
-  
+    
+    function populateBreedList() {
+        fetch(fetchUrl)
+        .then(res => res.json())
+        .then(data => {
+            dogBreeds = Object.keys(data.message); 
+            
+            dogBreeds.forEach(dogBreed => {
+                let breedItem = document.createElement('li');
+                let breedLink = document.createElement('a');
+                breedLink.href = "javascript:;";
+                breedLink.textContent = dogBreed;
+                breedItem.id = dogBreed.toLowerCase();
+
+                contentList.appendChild(breedItem); 
+                breedItem.appendChild(breedLink);
+
+                breedLink.addEventListener('click', handleImageDisplay);
+
+                function handleImageDisplay(e) {
+                    e.preventDefault();
+                    displayImageModal(dogBreed);
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+    }
+    populateBreedList();
+
+
+    function displayImageModal(dogBreed) {
+        const modal = document.createElement('div');
+        let url;
+        modal.classList.add('modal');
+    
+        modal.innerHTML = 
+            `<div class="modal-content">
+                <a href="javascript:;" class="close-modal" tabindex="0">Close</a>
+                <h3>${dogBreed}</h3>
+                <img src="" alt="${dogBreed}" id="modal-image">
+            </div>`;
+    
+        const modalImage = modal.querySelector('#modal-image');
+        
+        fetchRandomDogImage(dogBreed);
+        function fetchRandomDogImage(dogBreed) {
+            const fetchImageUrl = `https://dog.ceo/api/breed/${dogBreed}/images/random`;
+    
+            fetch(fetchImageUrl)
+            .then(res => res.json())
+            .then(data => {
+                url = data.message;
+                modalImage.src = url;
+            })
+            .catch(error => {
+                console.error(`Error fetching image for ${dogBreed}:`, error);
+                return null;
+            });
+        }
+    
+    
+        const closeModalButton = modal.querySelector('.close-modal');
+        closeModalButton.addEventListener('click', () => {
+            modal.style.display = 'none';
+            breedInput.focus();
+        });
+    
+        document.body.appendChild(modal);
+    }  
+
+    breedInput.addEventListener('input', handleFilter);   
+    function handleFilter() {
+        const searchText = breedInput.value.toLowerCase();
+
+        const breedItems = Array.from(contentList.children);
+        breedItems.forEach(breedItem => {
+            const dogBreed = breedItem.querySelector('a').textContent.toLowerCase();
+
+            if (dogBreed.includes(searchText)) {
+                breedItem.style.display = 'block';
+            } else {
+                breedItem.style.display = 'none';
+            }
+        });
+    }
+
+
+    breedInput.addEventListener('mouseover', mouseOver);
+    breedInput.addEventListener('mouseout', mouseOut);
+    function mouseOver() {
+        breedInput.classList.add('hover');
+    }
+    function mouseOut() {
+        breedInput.classList.remove('hover');
+    }
+
+
     function dogInformation() {
         fetch(fetchDogInfo)
         .then(res => res.json())
@@ -20,20 +118,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
             placeholderOption.textContent = 'Select a breed for more information.';
             breedSelect.appendChild(placeholderOption);
 
-            let breedName;
+            let dogBreed;
             let description;
             let hypoallergenic;
 
             dogFacts.forEach(dogFact => {
-                breedName = dogFact.attributes.name;
+                dogBreed = dogFact.attributes.name;
                 description = dogFact.attributes.description;
                 hypoallergenic = dogFact.attributes.hypoallergenic;
 
-              
-
                 const option = document.createElement('option');
-                option.value = breedName;
-                option.textContent = breedName;
+                option.value = dogBreed;
+                option.textContent = dogBreed;
                 breedSelect.appendChild(option);
 
             });
@@ -49,7 +145,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 const selectedDogFact = dogFacts.find(dogFact => dogFact.attributes.name === selectedBreed);
             
                 if (selectedDogFact) {
-                    breedName = selectedDogFact.attributes.name;
+                    dogBreed = selectedDogFact.attributes.name;
                     description = selectedDogFact.attributes.description;
                     hypoallergenic = selectedDogFact.attributes.hypoallergenic;
             
@@ -66,7 +162,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                         `
                              <a href="javascript:;" id="close-info" tabindex="0">Close</a>
                              <p>${description}</p>
-                             <p>The ${breedName} ${hypoallergenicText} hypoallergenic.</p>
+                             <p>The ${dogBreed} ${hypoallergenicText} hypoallergenic.</p>
                          `;
             
                     infoContainer.innerHTML = '';
@@ -86,110 +182,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
         });
     }
     dogInformation();
-    
-    function populateBreedList() {
-        fetch(fetchUrl)
-        .then(res => res.json())
-        .then(data => {
-            dogBreeds = Object.keys(data.message); 
-            
-            dogBreeds.forEach(breedName => {
-                let breedItem = document.createElement('li');
-                let breedLink = document.createElement('a');
-                breedLink.href = "javascript:;";
-                breedLink.textContent = breedName;
-                breedItem.id = breedName.toLowerCase();
-                contentList.appendChild(breedItem); 
-                breedItem.appendChild(breedLink);
-
-                breedLink.addEventListener('click', handleImageDisplay);
-
-                function handleImageDisplay(e) {
-                    e.preventDefault();
-                    displayImageModal(breedName);
-                }
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-    }
-    populateBreedList();
-
-    function fetchRandomDogImage(breedName) {
-        const imageUrl = `https://dog.ceo/api/breed/${breedName}/images/random`;
-
-        return fetch(imageUrl)
-        .then(res => res.json())
-        .then(data => {
-            return data.message;
-        })
-        .catch(error => {
-            console.error(`Error fetching image for ${breedName}:`, error);
-            return null;
-        });
-    }
-
-    function displayImageModal(breedName) {
-        const modal = document.createElement('div');
-        modal.classList.add('modal');
-        modal.innerHTML = 
-            `<div class="modal-content">
-                <a href="javascript:;" class="close-modal" tabindex="0">Close</a>
-                <h3>${breedName}</h3>
-                <img src="" alt="${breedName}" id="modal-image">
-            </div>`
-        ;
-    
-        const closeModalButton = modal.querySelector('.close-modal');
-        const modalImage = modal.querySelector('#modal-image');
-    
-        closeModalButton.addEventListener('click', () => {
-            modal.style.display = 'none';
-
-            breedInput.focus();
-        });
-
-        fetchRandomDogImage(breedName)
-            .then(imageUrl => {
-                if (imageUrl) {
-                    modalImage.src = imageUrl;
-                    modal.style.display = 'block';
-    
-                    closeModalButton.focus();
-                } else {
-                    alert('Failed to load image.');
-                }
-            });
-        document.body.appendChild(modal);
-    }
-    
-    function handleFilter() {
-        const searchText = breedInput.value.toLowerCase();
-
-        const breedItems = Array.from(contentList.children);
-        breedItems.forEach(breedItem => {
-            const breedName = breedItem.querySelector('a').textContent.toLowerCase();
-
-            if (breedName.includes(searchText)) {
-                breedItem.style.display = 'block';
-            } else {
-                breedItem.style.display = 'none';
-            }
-        });
-    }
-
-    breedInput.addEventListener('input', handleFilter);
-
-    breedInput.addEventListener('mouseover', mouseOver);
-    breedInput.addEventListener('mouseout', mouseOut);
-
-    function mouseOver() {
-        breedInput.classList.add('hover');
-    }
-    function mouseOut() {
-        breedInput.classList.remove('hover');
-    }
 
     const toTop = document.getElementById('backToTop');
     toTop.addEventListener('click', handleScroll);
